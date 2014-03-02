0bced8c10529fa735033c4f5a75d320a464bd86b
thoqws <thw@live.fi>
2014-03-03 00:51:26 +0200

    first commit with the rails app default stuff

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Storythings::Application.config.secret_key_base = '9b0d9be04518e0d0ec223181a4f8df77ce7647a75e082214022a6444ce805d1a6f436082aa015657586dd3ce314a1bad72fa458dac57ca10ac33b6ee3a393e57'
