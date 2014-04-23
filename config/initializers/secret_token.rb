f59a9ffcc6d85ead05644d0d4a76ad1877541662
thowes <thw@live.fi>
2014-04-23 23:53:08 +0300

    Add new app stuff

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Storythings::Application.config.secret_key_base = 'a43c7e2025bf30487f1b927e7c9657e12f3b049a555e23b1b275d3afbc54265b13a0d240091465f55263643cb88f3ed462cfdd85d62c69ab2e3e29aeda8c46ae'
