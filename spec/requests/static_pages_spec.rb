ec385869548af751912626191ba7a5e56aba6898
thowes <thw@live.fi>
2014-04-30 02:42:32 +0300

    Adding links to layout

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -4,45 +4,49 @@ describe "Static pages" do
 
   describe "Home page" do
     it "should have the content 'Storythings'" do
-      visit '/static_pages/home'
+      visit root_path
       expect(page).to have_content('Storythings')
     end
-    it "should have the title 'Home'" do
-      visit '/static_pages/home'
+    it "should have the base title" do
+      visit root_path
       expect(page).to have_title("Storythings")
+    end
+    it "should not have a custom page title" do
+      visit root_path
+      expect(page).not_to have_title('| Home')
     end
   end
 
   describe "Help page" do
     it "should have the content 'Help'" do
-      visit '/static_pages/help'
+      visit help_path
       expect(page).to have_content('Help')
     end
     it "should have the title 'Help'" do
-      visit '/static_pages/help'
+      visit help_path
       expect(page).to have_title("Storythings | Help")
     end
   end
 
   describe "About page" do
     it "should have the content 'About Us'" do
-      visit '/static_pages/about'
+      visit about_path
       expect(page).to have_content('About Us')
     end
     it "should have the title 'About Us'" do
-      visit '/static_pages/about'
+      visit about_path
       expect(page).to have_title("Storythings | About Us")
     end
   end
 
   describe "Contact page" do
     it "should have the content 'Contact'" do
-      visit '/static_pages/contact'
+      visit contact_path
       expect(page).to have_content('Contact')
     end
     it "should have the title 'Contact'" do
-      visit '/static_pages/about'
-      expect(page).to have_title("Storythings | About Us")
+      visit contact_path
+      expect(page).to have_title("Storythings | Contact")
     end
   end
 end
