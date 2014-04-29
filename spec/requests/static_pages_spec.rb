9ec8fb83dda61d15e12c5dbcf1c2cd45bd7b3a5c
thowes <thw@live.fi>
2014-04-29 19:41:15 +0300

    Finish StaticPages

---
+++ spec/requests/static_pages_spec.rb
@@ -0,0 +1,48 @@
+require 'spec_helper'
+
+describe "Static pages" do
+
+  describe "Home page" do
+    it "should have the content 'Storythings'" do
+      visit '/static_pages/home'
+      expect(page).to have_content('Storythings')
+    end
+    it "should have the title 'Home'" do
+      visit '/static_pages/home'
+      expect(page).to have_title("Storythings | Home")
+    end
+  end
+
+  describe "Help page" do
+    it "should have the content 'Help'" do
+      visit '/static_pages/help'
+      expect(page).to have_content('Help')
+    end
+    it "should have the title 'Help'" do
+      visit '/static_pages/help'
+      expect(page).to have_title("Storythings | Help")
+    end
+  end
+
+  describe "About page" do
+    it "should have the content 'About Us'" do
+      visit '/static_pages/about'
+      expect(page).to have_content('About Us')
+    end
+    it "should have the title 'About Us'" do
+      visit '/static_pages/about'
+      expect(page).to have_title("Storythings | About Us")
+    end
+  end
+
+  describe "Contact page" do
+    it "should have the content 'Contact'" do
+      visit '/static_pages/contact'
+      expect(page).to have_content('Contact')
+    end
+    it "should have the title 'Contact'" do
+      visit '/static_pages/about'
+      expect(page).to have_title("Storythings | About Us")
+    end
+  end
+end
