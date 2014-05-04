97ea3e6ab4f612ffc751f01591107ca3037ede24
thowes <thw@live.fi>
2014-05-04 23:06:32 +0300

    Adding secure password check

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,13 +2,16 @@ require 'spec_helper'
 
 describe User do
   before do
-    @user = User.new(name: "Example User", email: "user@example.com")
+    @user = User.new(name: "Example User", email: "user@example.com",
+                     password: "foobar", password_confirmation: "foobar")
   end
   subject { @user }
 
   it { should respond_to(:name) }
   it { should respond_to(:email) }
   it { should respond_to(:password_digest) }
+  it { should respond_to(:password) }
+  it { should respond_to(:password_confirmation) }
   it { should be_valid }
 
   describe "when name is not present" do
@@ -49,6 +52,18 @@ describe User do
       user_with_same_email = @user.dup
       user_with_same_email.save
     end
+    it { should_not be_valid }
+  end
+
+  describe "when password is not present" do
+    before do
+      @user = User.new(name: "Example User", email: "user@example.com",
+                     password: " ", password_confirmation: " ")
+    end
+    it { should_not be_valid }
+  end
+  describe "when password doesn't match confirmation" do
+    before { @user.password_confirmation = "mismatch" }
     it { should_not be_valid }
   end
 
