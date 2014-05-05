c59f09ecbad2edf720b8b839358cfda7c9b74605
thowes <thw@live.fi>
2014-05-05 09:40:16 +0300

    Fixing valid user signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,11 +7,10 @@ class UsersController < ApplicationController
 		@user = User.new
 	end
 
-
 	def create
     	@user = User.new(user_params)
     	if @user.save
-      		# Handle a successful save.
+      		redirect_to @user
     	else
       		render 'new'
     	end
