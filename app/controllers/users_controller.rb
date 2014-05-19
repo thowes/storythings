5dd543c0bf6e9cbfdec6798887c917e7f27f1381
thowes <thw@live.fi>
2014-05-19 10:42:15 +0300

    Adding signin when signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,8 +10,9 @@ class UsersController < ApplicationController
 	def create
 			@user = User.new(user_params)
 			if @user.save
+				sign_in @user
 				flash[:success] = "Welcome to the Storythings!"
-					redirect_to @user
+				redirect_to @user
 			else
 					render 'new'
 			end
