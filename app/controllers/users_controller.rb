4a69107cee9d671e0c859d5aa531e8eb846af2ba
thowes <thw@live.fi>
2014-05-25 00:55:51 +0300

    Requiring rights user for editing and updating

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
 	before_action :signed_in_user, only: [:edit, :update]
-	
+	before_action :correct_user,   only: [:edit, :update]
+
 	def show
 		@user = User.find(params[:id])
 	end
@@ -21,11 +22,11 @@ class UsersController < ApplicationController
 	end
 
 	def edit
-		@user = User.find(params[:id])
+		#@user = User.find(params[:id])
 	end
 
 	def update
-		@user = User.find(params[:id])
+		#@user = User.find(params[:id])
 		if @user.update_attributes(user_params)
 			# Handle a successful update.
 			flash[:success] = "Profile updated"
@@ -45,6 +46,11 @@ class UsersController < ApplicationController
 
 		def signed_in_user
 			redirect_to signin_url, notice: "Please sign in." unless signed_in?
+		end
+
+		def correct_user
+			@user = User.find(params[:id])
+			redirect_to(root_url) unless current_user?(@user)
 		end
 
 end
