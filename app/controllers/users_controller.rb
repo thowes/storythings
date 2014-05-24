2c0be7ec369499511090c19b5bbdb3a7b20bcae9
thowes <thw@live.fi>
2014-05-25 00:25:07 +0300

    Adding edit user page and fucntionality, with testing.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,19 +8,34 @@ class UsersController < ApplicationController
 	end
 
 	def create
-			@user = User.new(user_params)
-			if @user.save
-				sign_in @user
-				flash[:success] = "Welcome to the Storythings!"
-				redirect_to @user
-			else
-					render 'new'
-			end
+		@user = User.new(user_params)
+		if @user.save
+			sign_in @user
+			flash[:success] = "Welcome to the Storythings!"
+			redirect_to @user
+		else
+			render 'new'
 		end
+	end
 
-		private
-			def user_params
-					params.require(:user).permit(:name, :email, :password,
+	def edit
+		@user = User.find(params[:id])
+	end
+
+	def update
+		@user = User.find(params[:id])
+		if @user.update_attributes(user_params)
+			# Handle a successful update.
+			flash[:success] = "Profile updated"
+			redirect_to @user
+		else
+			render 'edit'
+		end
+	end
+	
+	private
+		def user_params
+			params.require(:user).permit(:name, :email, :password,
 																	 :password_confirmation)
-			end
+		end
 end
--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -11,6 +11,7 @@ describe "Authentication" do
 
 	describe "signin" do
 		before { visit signin_path }
+
 		describe "with invalid information" do
 			before { click_button "Sign in" }
 			it { should have_title('Sign in') }
@@ -20,15 +21,18 @@ describe "Authentication" do
 				it { should_not have_selector('div.alert.alert-error') }
 			end
 		end
+
 		describe "with valid information" do
 			let(:user) { FactoryGirl.create(:user) }
-			before do
-				fill_in "Email",    with: user.email.upcase
-				fill_in "Password", with: user.password
-				click_button "Sign in"
-			end
+			before { sign_in user }
+			#before do
+			#	fill_in "Email",    with: user.email.upcase
+			#	fill_in "Password", with: user.password
+			#	click_button "Sign in"
+			#end
 			it { should have_title(user.name) }
 			it { should have_link('Profile',     href: user_path(user)) }
+			it { should have_link('Settings',    href: edit_user_path(user)) }
 			it { should have_link('Sign out',    href: signout_path) }
 			it { should_not have_link('Sign in', href: signin_path) }
 
