5d3d183ded03be6feb4ad4b5982028fcb1a96a31
thowes <thw@live.fi>
2014-06-10 17:32:27 +0300

    Adding tetsing item model

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -4,7 +4,7 @@ describe Item do
   let(:user) { FactoryGirl.create(:user) }
   before do
     # This code is not idiomatically correct.
-    @item = Item.new(name: "Testiesine", user_id: user.id)
+    @item = Item.new(name: "Old Laptop", user_id: user.id)
   end
 
   subject { @item }
