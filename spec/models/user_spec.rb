05f24845a0624cf9560b1296a742cd1b1fdc59c4
thowes <thw@live.fi>
2014-06-10 17:03:51 +0300

    Adding status feed, with testing

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -145,10 +145,22 @@ describe User do
 			let(:unfollowed_post) do
 				FactoryGirl.create(:micropost, user: FactoryGirl.create(:user))
 			end
+			let(:followed_user) { FactoryGirl.create(:user) }
+			before do
+				@user.follow!(followed_user)
+				3.times { followed_user.microposts.create!(content: "Lorem ipsum") }
+			end
 			its(:feed) { should include(newer_micropost) }
 			its(:feed) { should include(older_micropost) }
 			its(:feed) { should_not include(unfollowed_post) }
+			its(:feed) do
+				followed_user.microposts.each do |micropost|
+					should include(micropost)
+				end
+			end
 		end
+
+
 	end
 
 	describe "following" do
@@ -161,11 +173,11 @@ describe User do
 		it { should be_following(other_user) }
 		its(:followed_users) { should include(other_user) }
 
-    describe "followed user" do
-      subject { other_user }
-      its(:followers) { should include(@user) }
-    end
-    
+		describe "followed user" do
+			subject { other_user }
+			its(:followers) { should include(@user) }
+		end
+		
 		describe "and unfollowing" do
 			before { @user.unfollow!(other_user) }
 
