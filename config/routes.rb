7e7697de393374b116f11a0cc211834a51f68629
thowes <thw@live.fi>
2014-06-10 12:27:10 +0300

    Adding follow and unfollow partials

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Storythings::Application.routes.draw do
 	end
 	resources :sessions, only: [:new, :create, :destroy]
 	resources :microposts, only: [:create, :destroy]
+	resources :relationships, only: [:create, :destroy]
 	root  'static_pages#home'
 	match '/signup',  to: 'users#new',            via: 'get'
 	match '/signin',  to: 'sessions#new',         via: 'get'
