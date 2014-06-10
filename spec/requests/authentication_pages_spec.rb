33b32e57287138490932c3f8580f8614532b230e
thowes <thw@live.fi>
2014-06-10 15:58:12 +0300

    Adding working follow button

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -47,8 +47,7 @@ describe "Authentication" do
 		end
 	end
 
-describe "authorization" do
-
+	describe "authorization" do
 		describe "for non-signed-in users" do
 			let(:user) { FactoryGirl.create(:user) }
 			describe "when attempting to visit a protected page" do
@@ -107,6 +106,16 @@ describe "authorization" do
 				end
 				describe "submitting to the destroy action" do
 					before { delete micropost_path(FactoryGirl.create(:micropost)) }
+					specify { expect(response).to redirect_to(signin_path) }
+				end
+			end
+			describe "in the Relationships controller" do
+				describe "submitting to the create action" do
+					before { post relationships_path }
+					specify { expect(response).to redirect_to(signin_path) }
+				end
+				describe "submitting to the destroy action" do
+					before { delete relationship_path(1) }
 					specify { expect(response).to redirect_to(signin_path) }
 				end
 			end
