3428d9fca708fdc7d061802e7143e7734d66f0d7
thowes <thw@live.fi>
2014-07-01 23:33:52 +0300

    some small changes to controllers code formatting

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
 	before_action :signed_in_user, only: [:index, :edit, :update, :destroy, :following, :followers]
+	#before_action :signed_in_user, except: [:show, :create]
 	before_action :correct_user,   only: [:edit, :update]
 	before_action :admin_user,     only: :destroy
 
