25dd5632743ce5d0c67a8c5ca6f094a27489d0d4
thowes <thw@live.fi>
2014-07-09 00:56:21 +0300

    changing app site title

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,7 +2,7 @@ module ApplicationHelper
 
 	# Returns the full title on a per-page basis.
 	def full_title(page_title)
-		base_title = "storythings"
+		base_title = "storythin.gs"
 		if page_title.empty?
 			base_title
 		else
