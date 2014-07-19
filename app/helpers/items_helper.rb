25ec8fef28734eeac056a3644c909b236448d8a8
thowes <thw@live.fi>
2014-07-19 19:45:02 +0300

    adding qr code tags for Items

--- app/helpers/items_helper.rb
+++ app/helpers/items_helper.rb
@@ -3,7 +3,7 @@ module ItemsHelper
   def qrcode_for(item, options = { size: 50 })
     qrcode_id = item_url
     qrcode_size = options[:size]
-    qrcode_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}"
+    qrcode_url = "https://chart.googleapis.com/chart?cht=qr&chl=#{qrcode_id}&chs=180x180&choe=UTF-8&chld=L|2"
     image_tag(qrcode_url, alt: item.name, class: "qrcode")
   end
 
