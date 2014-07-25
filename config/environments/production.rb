0284c85a28f502b0179d54e9592391ff0493f0ec
thowes <thw@live.fi>
2014-07-25 03:50:33 +0300

    changing stylesheet assets

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,4 +77,6 @@ Storythings::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+
+  config.assets.precompile += %w( vendor/modernizr.js )
 end
