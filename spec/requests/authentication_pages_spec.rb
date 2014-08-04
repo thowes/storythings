43adc43b3915053629b137f0d515d7380dd749af
thowes <thw@live.fi>
2014-08-04 23:54:20 +0300

    fixing layout

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -32,7 +32,6 @@ describe "Authentication" do
 			it { should have_title(user.name) }
 			it { should have_link('Other Users', href: users_path) }
 			it { should have_link('My Profile', href: user_path(user)) }
-			it { should have_link('Settings') }
 			it { should have_link('Edit Profile', href: edit_user_path(user)) }
 			it { should have_link('Sign out', href: signout_path) }
 			it { should_not have_link('Sign in', href: signin_path) }
