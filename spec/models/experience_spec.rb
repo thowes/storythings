46bfcfc32f7a4e858545f64fb397d9a6efa01075
thowes <thw@live.fi>
2015-02-11 14:37:31 +0200

    some testing for experience model

--- spec/models/experience_spec.rb
+++ spec/models/experience_spec.rb
@@ -1,5 +1,13 @@
 require 'spec_helper'
 
 describe Experience do
-  pending "add some examples to (or delete) #{__FILE__}"
+	before do
+		@experience = Experience.new(title: "Xp 1", description: "Testing")
+	end
+	subject { @experience }
+
+	it { should respond_to(:title) }
+	it { should respond_to(:description) }
+	it { should respond_to(:user_id) }
+	it { should respond_to(:quest_id) }
 end
