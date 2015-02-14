15426a74b039e3cc322d7d4fdd0143999af6c45e
thowes <thw@live.fi>
2015-02-14 23:11:08 +0200

    updating experience model

--- app/models/experience.rb
+++ app/models/experience.rb
@@ -1,2 +1,4 @@
 class Experience < ActiveRecord::Base
+	#belongs_to :user
+	validates :title, presence: true, length: { maximum: 100, minimum: 10 }
 end
--- spec/models/experience_spec.rb
+++ spec/models/experience_spec.rb
@@ -2,7 +2,9 @@ require 'spec_helper'
 
 describe Experience do
 	before do
-		@experience = Experience.new(title: "Xp 1", description: "Testing")
+		@user = User.new(name: "Example User", email: "user@example.com", password: "foobar", password_confirmation: "foobar")
+		@quest = Quest.new(name: "Example Quest", qtype: "QA")
+		@experience = Experience.new(title: "Test Experience 1", description: "This is testing the experience class model,", user_id: @user, quest_id: @quest)
 	end
 	subject { @experience }
 
@@ -10,4 +12,14 @@ describe Experience do
 	it { should respond_to(:description) }
 	it { should respond_to(:user_id) }
 	it { should respond_to(:quest_id) }
+
+	describe "with title blank" do
+		before { @experience.title = " " }
+		it { should_not be_valid }
+	end
+	describe "with title that is too long" do
+		before { @experience.title = "a" * 101 }
+		it { should_not be_valid }
+	end
+
 end
