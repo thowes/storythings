66a2f5797201ad6dc94d7329412007743d7e2558
thowes <thw@live.fi>
2015-03-16 23:55:26 +0200

    simplified top menu

--- spec/requests/auth_req_spec.rb
+++ spec/requests/auth_req_spec.rb
@@ -30,8 +30,8 @@ describe "Authentication" do
 			let(:user) { FactoryGirl.create(:user) }
 			before { sign_in user }
 			it { should have_title(user.name) }
-			it { should have_link('My Profile', href: user_path(user)) }
-			it { should have_link('Edit Profile', href: edit_user_path(user)) }
+			#it { should have_link('My Profile', href: user_path(user)) }
+			#it { should have_link('Edit Profile', href: edit_user_path(user)) }
 			it { should have_link('Sign out', href: signout_path) }
 			it { should_not have_link('Sign in', href: signin_path) }
 			describe "followed by signout" do
